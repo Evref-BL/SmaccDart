afd9452a880d9df17849ce5d9dfb495cda034d92
Nicolas Hlad <nicolas.hlad@berger-levrault.com>
2023-02-02 18:58:25 +0100

    add debug.dart for test

--- test/debug.dart
+++ test/debug.dart
@@ -1,3 +1,23 @@
-// generated code by Pharo
-// ignore_for_file: unnecessary_new
-export 'unsupported.dart';
+String str1 = """str1
+  d${tret.gtgt}:$nz
+testestest2
+testestest3
+""";
+
+String str2 =       """str2
+  testestest1
+  testestest2
+testestest3'''jlkjk'''
+""";
+
+String str3 =      '''str3
+  testestest1
+  test $estest2
+testestest3
+''';
+
+String str3 = '''str4
+  testestest1
+  test """gdtr"""
+testestest3
+''';
